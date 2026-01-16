8d4e5722187a32b38b87780d0a3b7e972de0dcb2
Xan <XanderOlson@users.noreply.github.com>
2026-01-15 23:20:09 -0800

    Add medical XP for tend heals

--- MMOCoreORB/src/server/zone/objects/creature/commands/TendCommand.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/TendCommand.h
@@ -234,6 +234,9 @@ public:
 			int healedAction = creatureTarget->healDamage(creature, CreatureAttribute::ACTION, healPower, true, false);
 
 			sendHealMessage(creature, creatureTarget, healedHealth, healedAction);
+
+			if (healedHealth + healedAction > 0)
+				awardXp(creature, "medical", round((healedHealth + healedAction) * 2.5f));
 		} else if (tendWound) {
 			uint8 attribute = CreatureAttribute::UNKNOWN;
 
@@ -269,7 +272,7 @@ public:
 
 			sendWoundMessage(creature, creatureTarget, attribute, healedWounds);
 
-			if (creatureTarget != creature && healedWounds > 0) {
+			if (healedWounds > 0) {
 				awardXp(creature, "medical", round(healedWounds * 2.5f));
 				creature->notifyObservers(ObserverEventType::ABILITYUSED, creatureTarget, STRING_HASHCODE("tendwound"));
 			}
