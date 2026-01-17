9957f87dad2bfafce8db0867967dc922010a0bb0
Xan <XanderOlson@users.noreply.github.com>
2026-01-17 08:47:28 -0800

    Reduce old man and sith delays to 10 minutes

--- MMOCoreORB/bin/scripts/screenplays/village/intro/fs_intro.lua
+++ MMOCoreORB/bin/scripts/screenplays/village/intro/fs_intro.lua
@@ -12,8 +12,8 @@ FsIntro = ScreenPlay:new {
 	VILLAGE = 8,
 
 	stepDelay = {
-		[1] = { 43200, 129600 }, -- Old man visit, 12-36 hours
-		[3] = { 3600, 43200 } -- Sith shadow attack, 1 hour to 12 hours
+		[1] = { 600, 600 }, -- Old man visit, 10 minutes
+		[3] = { 600, 600 } -- Sith shadow attack, 10 minutes
 	}
 }
 
